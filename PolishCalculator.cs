67b2ce144f20dfbb2a63ad4dace705cef46c598f
eduard <kuchmel.eded@mail.ru>
2022-03-01 22:26:02 +0300

    Refactoring

---
+++ PolishCalculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorEpt
+{
+
+    internal class PolishCalculator
+    {
+        //Тело польского калькулятора
+        public static void CalculatingTheResultOfSortedExpression(string[] sortedExpression)
+        {
+            Stack<double> stackOfOperandsToPerform = new();
+            for (int i = 0; i < sortedExpression.Length; i++)
+            {
+                if (int.TryParse(sortedExpression[i], out int r))
+                {
+                    stackOfOperandsToPerform.Push(r);
+                }
+
+                else
+                {
+                    double temp1;
+                    double temp2;
+                    switch (sortedExpression[i])
+                    {
+                        case "+":
+                            stackOfOperandsToPerform.Push(stackOfOperandsToPerform.Pop() + stackOfOperandsToPerform.Pop()); break;
+
+                        case "-":
+                            temp1 = stackOfOperandsToPerform.Pop();
+                            temp2 = stackOfOperandsToPerform.Pop();
+                            stackOfOperandsToPerform.Push(temp2 - temp1);
+                            break;
+
+                        case "*":
+                            stackOfOperandsToPerform.Push(stackOfOperandsToPerform.Pop() * stackOfOperandsToPerform.Pop()); break;
+
+                        case "/":
+                            temp1 = stackOfOperandsToPerform.Pop();
+                            temp2 = stackOfOperandsToPerform.Pop();
+                            stackOfOperandsToPerform.Push(temp2 / temp1);
+                            break;
+
+                        case "^":
+                            temp1 = stackOfOperandsToPerform.Pop();
+                            temp2 = stackOfOperandsToPerform.Pop();
+                            stackOfOperandsToPerform.Push(Math.Pow(temp2, temp1));
+                            break;
+                    }
+                }
+            }
+
+            double result = stackOfOperandsToPerform.Pop();
+            Console.WriteLine(result);
+        }
+    }
+
+    //Выполнение математических операций над операндами
+    public static void PerformingMathematicalOperations ()
+    {
+
+    }
+}
---
+++ SortingStation.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorEpt
+{
+    //Сортировочная станция, перевод обычной записи в обратную польскую
+    public static class SortingStation
+    {
+        //Тело сортировочной станции
+        public static string[] Sortingstation(string unsortedExpression)
+        {
+            string numberCell = string.Empty;
+            Stack<char> operatorsStack = new();
+            Queue<string> output = new();
+            for (int i = 0; i < unsortedExpression.Length; i++)
+            {
+                if (char.IsDigit(unsortedExpression[i]))
+                {
+                    numberCell += unsortedExpression[i];
+                    continue;
+                }
+
+                else
+                {
+                    output.Enqueue(numberCell);
+                    numberCell = string.Empty;
+                    WorkingWithOperators(unsortedExpression[i], operatorsStack, output);
+                }
+
+            }
+            output.Enqueue(numberCell);  
+            foreach (char c in operatorsStack)
+            {
+                output.Enqueue(c.ToString());
+            }
+            return output.Where(n => n.Length != 0).ToArray();
+        }
+
+        //Сравнивание приоритета операторов и распихивание их по стекам/очередям
+        public static void WorkingWithOperators(char currentoperator, Stack<char> operatorsStack, Queue<string> outputQueue)
+        {
+            while (true)
+            {
+                if (currentoperator == ')' && operatorsStack.Peek() == '(')
+                {
+                    operatorsStack.Pop();
+                    break;
+                }
+
+                if (currentoperator == '(')
+                {
+                    operatorsStack.Push(currentoperator);
+                    break;
+                }
+
+                else if (currentoperator == ')')
+                {
+                    outputQueue.Enqueue(Convert.ToString(operatorsStack.Pop()));
+                }
+
+                else if (operatorsStack.Count == 0)
+                {
+                    operatorsStack.Push(currentoperator);
+                    break;
+                }
+
+                else
+                {
+                    if (GetPrecendenceOfOperands(currentoperator) > GetPrecendenceOfOperands(operatorsStack.Peek()))
+                    {
+                        operatorsStack.Push(currentoperator);
+                        break;
+                    }
+
+                    if (GetPrecendenceOfOperands(currentoperator) == GetPrecendenceOfOperands(operatorsStack.Peek()))
+                    {
+                        outputQueue.Enqueue(Convert.ToString(operatorsStack.Pop()));
+                        operatorsStack.Push(currentoperator);
+                        break;
+                    }
+
+                    if ((GetPrecendenceOfOperands(currentoperator) < GetPrecendenceOfOperands(operatorsStack.Peek())))
+                    {
+                        outputQueue.Enqueue(Convert.ToString(operatorsStack.Pop()));
+                    }
+                }
+            }
+        }
+
+        //Получение приоритета операторов и операндов для сортировки
+        public static int GetPrecendenceOfOperands(char symbol)
+        {
+            return symbol switch
+            {
+                '^' => 3,
+                '*' => 2,
+                '/' => 2,
+                '+' => 1,
+                '-' => 1,
+                _ => 0,
+            };
+        }
+    }
+}
